64f8feeedbfdc36d2ee7ac466551aa0ae9991560
mc704ed <mykyta.churylov@student.tuke.sk>
2026-04-02 13:35:27 +0200

    ff

--- src/main/kotlin/mate/academy/FinancialService.kt
+++ src/main/kotlin/mate/academy/FinancialService.kt
@@ -8,7 +8,9 @@ class FinancialService {
         currencyCode: CurrencyCode,
         transactionId: TransactionId
     ) : String {
-        // TODO: implement
+        return "Transferred ${amount.amount} " +
+                "${currencyCode.code} from ${source.value} to ${destination.value}. " +
+                "Transaction ID: ${transactionId.value}"
     }
 
     fun convertCurrency(
@@ -16,7 +18,10 @@ class FinancialService {
         fromCurrency: CurrencyCode,
         toCurrency: CurrencyCode
     ): CurrencyAmount {
-        // TODO: implement
+        val exchange = getExchangeRate(fromCurrency, toCurrency)
+        val result = amount.amount * exchange
+        val currency: CurrencyAmount = CurrencyAmount(result)
+        return currency
     }
 
     private fun getExchangeRate(fromCurrency: CurrencyCode, toCurrency: CurrencyCode): Double {
@@ -26,5 +31,29 @@ class FinancialService {
             fromCurrency.code == "USD" && toCurrency.code == "GBP" -> 0.82
             else -> 1.0
         }
+    }
+}
+@JvmInline
+value class AccountNumber(val value: String) {
+    init {
+        require(value.length == 10 && value.all { it.isDigit() }) {"Invalid input"}
+    }
+}
+@JvmInline
+value class CurrencyAmount(val amount: Double) {
+    init {
+        require(amount >= 0) {"Invalid input"}
+    }
+}
+@JvmInline
+value class CurrencyCode(val code: String) {
+    init {
+        require(code.length == 3 && code == code.uppercase()) {"Invalid input"}
+    }
+}
+@JvmInline
+value class TransactionId(val value: String?) {
+    init {
+        require(value != null && value.isNotEmpty()) {"Invalid input"}
     }
 }
